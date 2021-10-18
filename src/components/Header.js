2ba320c673323bc6e43fde5bd3edc2cc05574e03
albertorodriguezdiaz <alberto@efectovirtual.com>
2021-10-17 22:50:03 -0500

    Add style components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+
+const ContenedorHeader = styled.header`
+    background-color: #26c6da;
+    padding: 10px;
+    font-weight: bold;
+    color: #fff;
+`;
 
 const Header = ({titulo}) => {
     return ( 
-        <h1>{titulo}</h1>
+        <ContenedorHeader>
+            <h1>{titulo}</h1>
+        </ContenedorHeader>
      );
 }
  
